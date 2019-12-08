6048be3368d42a4ea97f01427c488a8e29688fce
Michael Jenis <michael.jenis@socialbakers.com>
2019-12-08 21:04:43 +0100

    insolvency widget, updated logic for rules/sponsorship, new selectors

--- src/pages/detail/detail.jsx
+++ src/pages/detail/detail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import {createStructuredSelector} from 'reselect'
 import { connect } from 'react-redux'
 import {loadDetail} from '../../redux/actions'
-import {getFullName, isDetailLoading, getDescription, getRoles, getDonations} from '../../redux/selectors'
+import {getFullName, isDetailLoading, getDescription, getRoles, getDonations, getPersonalInsolvency, getCompanyInsolvency} from '../../redux/selectors'
 import LoadingBar from '../../components/loadingBar/loadingBar'
 import styles from './detail.module.scss';
 
@@ -14,6 +14,20 @@ function TableRow(role) {
         <div>{role.value}</div>
       </div>
     </React.Fragment>
+  )
+}
+
+function WidgetInsolvency(insolvency) {
+  return (
+    <div>
+      <div>je věřitelem</div>
+      <div>
+        <div>jako fyzická osoba</div>
+        <div>{insolvency.personalInsolvency.count}</div>
+        <div>skrz právnickou osobu</div>
+        <div>{insolvency.companyInsolvency.count}</div>
+      </div>
+    </div>
   )
 }
 
@@ -50,14 +64,37 @@ function Detail(props) {
                 <div className={styles.detailBox}>
                   <h2>Role</h2>
                   <React.Fragment>
-                    {props.roles && props.roles.map((role, i) => <TableRow name={role.name} value={role.organization} key={i} />)}
+                    {props.roles && props.roles.map((group, index) => {
+                      return (
+                        <div key={index}>
+                          <div>{group.year}</div>
+                          {group.items.map((item, index) => <TableRow name={item.name} value={item.organization} key={index}/>)}
+                        </div>
+                      )
+                    })}
                   </React.Fragment>
                 </div>
                 <div className={styles.detailBox}>
                   <h2>Sponzorstvi</h2>
                   <React.Fragment>
-                    {props.donations && props.donations.map((donation, i) => <TableRow name={donation.party} value={donation.value} key={i} />)}
+                    {props.donations && props.donations.map((group, index) => {
+                      return (
+                        <div key={index}>
+                          <div>{group.year}</div>
+                          {group.items.map((item, index) => <TableRow name={item.party} value={item.value} key={index} />)}
+                        </div>
+                      )
+                    })}
                   </React.Fragment>
+                </div>
+                <div className={styles.detailBox}>
+                  <h2>Insolvence</h2>
+                  {props.personalInsolvency && props.companyInsolvency &&
+                    <React.Fragment>
+                      <WidgetInsolvency personalInsolvency={props.personalInsolvency.creditor} companyInsolvency={props.companyInsolvency.creditor}/>
+                      <WidgetInsolvency personalInsolvency={props.personalInsolvency.debtor} companyInsolvency={props.companyInsolvency.debtor}/>
+                      <WidgetInsolvency personalInsolvency={props.personalInsolvency.bailiff} companyInsolvency={props.companyInsolvency.bailiff}/>
+                    </React.Fragment>}
                 </div>
               </div>
             </div>
@@ -74,6 +111,8 @@ const mapStateToProps = createStructuredSelector({
   description: getDescription,
   roles: getRoles,
   donations: getDonations,
+  personalInsolvency: getPersonalInsolvency,
+  companyInsolvency: getCompanyInsolvency,
 })
 
 export default connect(mapStateToProps, {loadDetail})(Detail);
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,12 +3,41 @@ export const getSearchResults = store => store.app.searchResults
 export const isSearchLoading = store => store.app.loadingSearch
 export const isDetailLoading = store => store.app.loadingDetail
 export const getDetailData = store => store.app.detail
-export const getRoles = store => getDetailData(store).roles
-export const getDonations = store => getDetailData(store).donations
+export const getPersonalInsolvency = store => getDetailData(store).personalInsolvency
+export const getCompanyInsolvency = store => getDetailData(store).companyInsolvency
+
 export const getFullName = store => {
 	const detail = getDetailData(store)
 	return `${detail.titlePrefix} ${detail.firstName} ${detail.lastName} ${detail.titleSuffix}`.trim() // TODO lip naformatovat
 }
+
 export const getDescription = store => {
   return getDetailData(store).description
+}
+
+const groupByYear = (data) => {
+  if (!data) return []
+
+  let unsorted = data.reduce((groups, item) => {
+    groups[item.year] = groups[item.year] || {
+      year: item.year,
+      items: []
+    }
+    groups[item.year].items.push(item)
+    return groups
+  }, {})
+
+  return Object.values(unsorted).sort((a, b) => {
+    return b.year - a.year
+  })
+}
+
+export const getDonations = store => groupByYear(getDetailData(store).donations)
+
+export const getRoles = store => {
+  const detailData = getDetailData(store).roles
+  for(let i in detailData) {
+    detailData[i].year = detailData[i].startDate.substring(0,4)
+  }
+  return groupByYear(detailData)
 }
